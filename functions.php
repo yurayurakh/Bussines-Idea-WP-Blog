a5fd4ab08b902132b98a83f11aa50d28af961634
Yurii Yurakh <yurayurakh@gmail.com>
2017-10-14 20:46:34 +0300

    integrade theme

--- functions.php
+++ functions.php
@@ -7,93 +7,73 @@
  * @package Bussines_Idea
  */
 
-if ( ! function_exists( 'bussines_idea_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function bussines_idea_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on Bussines Idea, use a find and replace
-		 * to change 'bussines-idea' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'bussines-idea', get_template_directory() . '/languages' );
-
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
-
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'bussines-idea' ),
-		) );
-
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
-
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'bussines_idea_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
-endif;
-add_action( 'after_setup_theme', 'bussines_idea_setup' );
-
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
+
+Required: set 'ot_theme_mode' filter to true.
  */
-function bussines_idea_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'bussines_idea_content_width', 640 );
+add_filter( 'ot_theme_mode', '__return_true' );
+add_filter( 'ot_show_new_layout', '__return_false');
+/**
+
+Required: include OptionTree.
+ */
+require( trailingslashit( get_template_directory() ) . 'option-tree/ot-loader.php' );
+
+/** Theme Option */
+require( trailingslashit( get_template_directory() ) . 'template/meta-boxes.php' );
+require( trailingslashit( get_template_directory() ) . 'template/theme-options.php' );
+
+
+function bussines_idea_setup() {
+
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support( 'automatic-feed-links' );
+
+    /*
+     * Let WordPress manage the document title.
+     * By adding theme support, we declare that this theme does not use a
+     * hard-coded <title> tag in the document head, and expect WordPress to
+     * provide it for us.
+     */
+    add_theme_support( 'title-tag' );
+
+    /*
+     * Enable support for Post Thumbnails on posts and pages.
+     *
+     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+     */
+    add_theme_support( 'post-thumbnails' );
+
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus( array(
+        'menu-1' => esc_html__( 'Primary', 'bussines-idea' ),
+    ) );
+
+    /*
+     * Switch default core markup for search form, comment form, and comments
+     * to output valid HTML5.
+     */
+    add_theme_support( 'html5', array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ) );
+
+    /**
+     * Add support for core custom logo.
+     *
+     * @link https://codex.wordpress.org/Theme_Logo
+     */
+    add_theme_support( 'custom-logo', array(
+        'height'      => 250,
+        'width'       => 250,
+        'flex-width'  => true,
+        'flex-height' => true,
+    ) );
 }
-add_action( 'after_setup_theme', 'bussines_idea_content_width', 0 );
+add_action( 'after_setup_theme', 'bussines_idea_setup' );
 
 /**
  * Register widget area.
@@ -113,46 +93,34 @@ function bussines_idea_widgets_init() {
 }
 add_action( 'widgets_init', 'bussines_idea_widgets_init' );
 
-/**
- * Enqueue scripts and styles.
- */
+
+/* Enqueue Style **/
+
+function bussines_idea_style() {
+    wp_enqueue_style( 'bussines-idea-style', get_stylesheet_uri() );
+
+    wp_enqueue_style( 'OwlCarousel', get_template_directory_uri() . '/lib/OwlCarousel2/dist/assets/owl.theme.default.min.css', array('bussines-idea-style'));
+
+}
+
+add_action( 'wp_enqueue_scripts', 'bussines_idea_style' );
+
+/* Enqueue Scripts **/
+
 function bussines_idea_scripts() {
-	wp_enqueue_style( 'bussines-idea-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'bussines-idea-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'bussines-idea-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    //wp_enqueue_script( 'jquery2', get_template_directory_uri() . '/lib/jquery/jquery.min.js', array(''), '',true );
+    wp_enqueue_script( 'OwlCarousel', get_template_directory_uri() . '/lib/OwlCarousel2/dist/owl.carousel.min.js', array('jquery'), '', true );
+    wp_enqueue_script( 'bussines-idea-common-js', get_template_directory_uri() . '/js/common.js', array('jquery'), '',true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+
 }
+
 add_action( 'wp_enqueue_scripts', 'bussines_idea_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
 

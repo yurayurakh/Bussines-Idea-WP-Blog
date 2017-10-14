76e8ecb6477f30c9d6c389c6f116edd020c37a71
Yurii Yurakh <yurayurakh@gmail.com>
2017-10-14 22:13:31 +0300

    header edit

--- header.php
+++ header.php
@@ -22,35 +22,72 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bussines-idea' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 col-sm-4 col-xs-10">
+                    <a class="header__logo logo" href="/">
+                        <img src="img/logo_white.png" alt="logo header">
+                        <span class="logo__descr">- банк бизнес идей и планов</span>
+                    </a>
+                </div>
+                <div class="col-lg-6 col-md-7 col-sm-8 float-right">
+                    <button class="hamburger hidden-lg hidden-md hidden-sm">☰</button>
+                    <button class="cross hidden-lg hidden-md hidden-sm" style="display: none;">˟</button>
+                    <nav class="header__nav nav">
+                        <ul class="nav__list">
+                            <li class="nav__item">
+                                <a href="">
+                                    Главная
+                                </a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="">
+                                    Как добиться успеха в бизнесе
+                                </a>
+                            </li>
+                            <li class="nav__item">
+                                <a href="">
+                                    Образец бизнес-плана
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="search-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="titels">
+                        <?php if (is_front_page() && is_home()){ ?>
+                            <h1 class="main-title">
+                                <?php bloginfo('name') ?>
+                            </h1>
+                        <?php } else { ?>
+                            <h2 class="main-title">
+                                сайт свежих бизнес идей и планов
+                            </h2>
+                        <?php } ?>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bussines-idea' ); ?></button>
-			<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+                        <h2 class="sub-title">
+                            Бизнес-планы и бизнес-идеи для открытия своего дела с нуля
+                        </h2>
+                    </div>
+                    <form role="search" method="get" class="search-form" action="">
+                        <input type="search" id="" class="search-field" placeholder="Введите ваше сообщение ..." value="" name="s" />
+                        <button type="submit" class="search-submit"><span class="hidden-xs">Начать</span> поиск</button>
+                    </form>
+                    <div class="col-xs-6 right-text mobile-padding-del">
+                        <a class="blog__item-link" href="/">Бизнес-идеи (220 статей)</a>
+                    </div>
+                    <div class="col-xs-6 mobile-padding-del">
+                        <a class="blog__item-link" href="/">Бизнес-планы (70 статей)</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
